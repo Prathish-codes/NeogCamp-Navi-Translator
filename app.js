0d5aa4cd14d45fac6a1b8d0ac3e48a1c840c61c8
prathish-code <88712212+Prathish-codes@users.noreply.github.com>
2021-08-20 20:09:29 +0530

    UPDATED:NAVI TRANSLATOR

---
+++ app.js
@@ -0,0 +1,72 @@
+var btnTranslate =document.querySelector("#btn-translate")
+var txtInput =document.querySelector("#txt-input")
+var outputDiv= document.querySelector("#output")
+
+
+var serverURL=document.querySelector("#btn-translate")
+var txtInput =document.querySelector("#txt-input")
+var outputDiv= document.querySelector("#output")
+
+
+var serverURL =("https://api.funtranslations.com/translate/navi.json")
+
+
+
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+text
+}
+
+
+function errorHandler(text){
+    return serverURL("erorr occured" ,error);
+}
+
+
+function clickHander() {
+    var inputText=txtInput.value;///taking input
+
+    //calling server function for procssing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+         .then(json =>{
+          var translatedText = json.contents.translated;
+          outputDiv.innerText=translatedText;
+         })
+
+        .catch(errorHandler)  
+             
+};                
+
+////******click event */
+btnTranslate.addEventListener ("click" ,  clickHander)
+
+
+
+
+function getTranslationURL(text){
+    return serverURL +"?"+"text="+text
+}
+
+
+function errorHandler(text){
+    return serverURL("erorr occured" ,error);
+}
+
+
+function clickHander() {
+    var inputText=txtInput.value;///taking input
+
+    //calling server function for procssing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+         .then(json =>{
+          var translatedText = json.contents.translated;
+          outputDiv.innerText=translatedText;
+         })
+
+        .catch(errorHandler)  
+             
+};                
+
+////******click event */
+btnTranslate.addEventListener ("click" ,  clickHander)
